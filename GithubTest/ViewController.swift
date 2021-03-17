beb0914798f35c1a3a4b881732da16efaa8b6d0f
VasoBaramidze <vaso@lemondo.com>
2021-03-17 23:48:00 +0400

    adding label and todo mark

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//TODO: - this is todo mark
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
